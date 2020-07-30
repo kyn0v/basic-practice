a694c4f7958cb3b929bf9dbf387f8a155260cab2
kyn0v <2275169040@qq.com>
2020-07-30 21:11:17 +0800

    Typo

--- socket_program(win)/client_socket.cpp
+++ socket_program(win)/client_socket.cpp
@@ -1,141 +1,122 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>  
+
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib")
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 using namespace cv;
 
-enum SOCKETDATATYPE
-{
-	IMAGE_DATA = 1,	// ÕºœÒ÷°±Í÷æŒª#
-	CONTROL_DATA = 2 // øÿ÷∆–≈∫≈±Í÷æŒª*
+enum SOCKETDATATYPE {
+    IMAGE_DATA = 1,   // ÂõæÂÉèÊï∞ÊçÆ
+    CONTROL_DATA = 2  // ÊéßÂà∂Êï∞ÊçÆ
 };
 
+class SocketClientTCP {
+   public:
+    bool init(string ip, int port);
+    bool connnect();
+    bool transmit(char *data, int size, SOCKETDATATYPE type);
+    void disconnect();
+    void cleanup();
 
-class SocketClientTCP
-{
-public:
-	bool init(string ip, int port);
-	bool connnect();
-	void disconnect();
-	bool transmit(char *data, int size, SOCKETDATATYPE type);
-	void cleanup();
-private:
-	SOCKET clientSocket;
-	sockaddr_in sock_in;
+   private:
+    SOCKET clientSocket;
+    sockaddr_in sock_in;
 };
 
-bool SocketClientTCP::init(string ip, int port)
-{
-	WORD sockVersion = MAKEWORD(2, 2);
-	WSADATA data;
-	if (WSAStartup(sockVersion, &data) != 0)
-	{
-		printf("WSAStartup error!\n");
-		return false;
-	}
+bool SocketClientTCP::init(string ip, int port) {
+    WORD sockVersion = MAKEWORD(2, 2);
+    WSADATA data;
+    if (WSAStartup(sockVersion, &data) != 0) {
+        printf("WSAStartup error!\n");
+        return false;
+    }
 
-	sock_in.sin_family = AF_INET;
-	sock_in.sin_port = htons(port);
-	sock_in.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
-	return true;
+    sock_in.sin_family = AF_INET;
+    sock_in.sin_port = htons(port);
+    sock_in.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+    return true;
 }
 
-bool SocketClientTCP::connnect()
-{
-	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (clientSocket == INVALID_SOCKET) {
-		printf("create client error!\n");
-		return false;
-	}
-	if (connect(clientSocket, (sockaddr*)&sock_in, sizeof(sock_in)) == SOCKET_ERROR) {
-		printf("client connect error!\n");
-		return false;
-	}
-	return true;
+bool SocketClientTCP::connnect() {
+    clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        printf("create client error!\n");
+        return false;
+    }
+    if (connect(clientSocket, (sockaddr *)&sock_in, sizeof(sock_in)) == SOCKET_ERROR) {
+        printf("client connect error!\n");
+        return false;
+    }
+    return true;
 }
 
-void SocketClientTCP::disconnect()
-{
-	closesocket(clientSocket);
+void SocketClientTCP::disconnect() {
+    closesocket(clientSocket);
 }
 
-
-
-bool SocketClientTCP::transmit(char * data, int size, SOCKETDATATYPE type)
-{
-	if (type == IMAGE_DATA) {	//∑¢ÀÕÕºœÒ ˝æ›
-		char image_buffer[100000];
-		image_buffer[0] = '#';
-		for (int i = 0; i < size; i++)
-		{
-			image_buffer[i + 1] = data[i];
-		}
-		if (send(clientSocket, image_buffer, size + 1, 0) < 0) {
-			return false;
-		}
-	}
-	else if (type == CONTROL_DATA) {	//∑¢ÀÕøÿ÷∆–≈∫≈ ˝æ›
-		char control_buffer[100];
-		control_buffer[0] = '*';
-		for (int i = 0; i < size; i++)
-		{
-			control_buffer[i + 1] = data[i];
-		}
-		if (send(clientSocket, control_buffer, size + 1, 0) < 0) {
-			return false;
-		}
-	}
-	else {
-		printf("unknown SOCKETDATATYPE!\n");
-		return false;
-	}
-	return true;
+bool SocketClientTCP::transmit(char *data, int size, SOCKETDATATYPE type) {
+    if (type == IMAGE_DATA) {
+        char image_buffer[100000];
+        image_buffer[0] = '#';
+        for (int i = 0; i < size; i++) {
+            image_buffer[i + 1] = data[i];
+        }
+        if (send(clientSocket, image_buffer, size + 1, 0) < 0) {
+            return false;
+        }
+    } else if (type == CONTROL_DATA) {
+        char control_buffer[100];
+        control_buffer[0] = '*';
+        for (int i = 0; i < size; i++) {
+            control_buffer[i + 1] = data[i];
+        }
+        if (send(clientSocket, control_buffer, size + 1, 0) < 0) {
+            return false;
+        }
+    } else {
+        printf("unknown SOCKETDATATYPE!\n");
+        return false;
+    }
+    return true;
 }
 
-void SocketClientTCP::cleanup()
-{
-	WSACleanup();
+void SocketClientTCP::cleanup() {
+    WSACleanup();
 }
 
-int main()
-{
-	SocketClientTCP st;
-	st.init("192.168.0.105", 8086);
-	VideoCapture capture(0);//¥Úø™…„œÒÕ∑
-	Mat image;
-	while (true) {
-		st.connnect();
-		// ¥¶¿Ì ˝æ›
-		capture >> image;//∂¡»ÎÕº∆¨
-		cv::resize(image, image, Size(1280, 960));
-		if (image.empty())    //»Áπ˚’’∆¨Œ™ø’‘ÚÕÀ≥ˆ
-		{
-			printf("empty image\n\n");
-			return 0;
-		}
-		imshow("image", image);
-		waitKey(30);
-
-		std::vector<uchar> image_encode;
-		std::vector<int> quality;
-		quality.push_back(CV_IMWRITE_JPEG_QUALITY);
-		quality.push_back(50);//Ω¯––50%µƒ—πÀı
-		imencode(".jpg", image, image_encode, quality);
-
-		int num = image_encode.size();
-
-		char *data = reinterpret_cast<char*>(&image_encode[0]);
-
-		st.transmit(data, num, IMAGE_DATA);
-
-		waitKey(70);
-		st.disconnect();
-	}
-	st.cleanup();
-	return 0;
+int main() {
+    SocketClientTCP st;
+    st.init("192.168.0.105", 8086);
+    VideoCapture capture(0);
+    Mat image;
+    while (true) {
+        st.connnect();
+        // ÂáÜÂ§áÊï∞ÊçÆ
+        capture >> image;
+        cv::resize(image, image, Size(1280, 960));
+        if (image.empty()) {
+            printf("empty image\n\n");
+            return 0;
+        }
+        imshow("image", image);
+        waitKey(30);
+        std::vector<uchar> image_encode;
+        std::vector<int> quality;
+        quality.push_back(CV_IMWRITE_JPEG_QUALITY);
+        quality.push_back(50);  // ÂéãÁº©Áéá50%
+        imencode(".jpg", image, image_encode, quality);
+        int num = image_encode.size();
+        char *data = reinterpret_cast<char *>(&image_encode[0]);
+        // ‰º†ËæìÁºñÁ†ÅÊï∞ÊçÆ
+        st.transmit(data, num, IMAGE_DATA);
+        st.disconnect();
+    }
+    st.cleanup();
+    return 0;
 }
--- socket_program(win)/server_socket.cpp
+++ socket_program(win)/server_socket.cpp
@@ -1,142 +1,135 @@
-#define  _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <iostream>
-#include <string>
-#include <opencv2/opencv.hpp>  
+
 #include <winsock2.h>
-#pragma comment(lib,"ws2_32.lib")
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 using namespace cv;
 
-enum SOCKETDATATYPE
-{
-	IMAGE_DATA = 1,	// ÕºœÒ÷°±Í÷æŒª#
-	CONTROL_DATA = 2 // øÿ÷∆–≈∫≈±Í÷æŒª*
+enum SOCKETDATATYPE {
+    IMAGE_DATA = 1,   // ÂõæÂÉèÊï∞ÊçÆ
+    CONTROL_DATA = 2  // ÊéßÂà∂Êï∞ÊçÆ
 };
 
-class SocketServerTCP
-{
-public:
-	bool init(int port);
-	bool connect();
-	void disconnect();
-	bool receive(char buffer[], int buffer_size, SOCKETDATATYPE &type, int &size);
-	void cleanup();
-private:
-	SOCKET serverSocket;
-	SOCKET connectionSocket;
+class SocketServerTCP {
+   public:
+    bool init(int port);
+    bool connect();
+    bool receive(char *buffer, int buffer_size, SOCKETDATATYPE &type, int &size);
+    void disconnect();
+    void cleanup();
+
+   private:
+    SOCKET serverSocket;
+    SOCKET connectionSocket;
 };
 
-bool SocketServerTCP::init(int port)
-{
-	WORD sockVersion = MAKEWORD(2, 2);
-	WSADATA wsdata;
-	if (WSAStartup(sockVersion, &wsdata) != 0)
-	{
-		printf("WSAStartup error!\n");
-		return false;
-	}
-	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (serverSocket == INVALID_SOCKET)
-	{
-		printf("create server error!\n");
-		return false;
-	}
-	sockaddr_in sockAddr;
-	sockAddr.sin_family = AF_INET;
-	sockAddr.sin_port = htons(port);
-	sockAddr.sin_addr.S_un.S_addr = INADDR_ANY;
-	if (bind(serverSocket, (sockaddr*)&sockAddr, sizeof(sockAddr)) == SOCKET_ERROR) {
-		printf("bind server error!\n");
-		return false;
-	}
-	if (listen(serverSocket, 10) == SOCKET_ERROR) {
-		printf("create server listenner error!\n");
-		return false;
-	}
-	return true;
+bool SocketServerTCP::init(int port) {
+    WORD sockVersion = MAKEWORD(2, 2);
+    WSADATA wsdata;
+    if (WSAStartup(sockVersion, &wsdata) != 0) {
+        printf("WSAStartup error!\n");
+        return false;
+    }
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        printf("create server error!\n");
+        return false;
+    }
+    sockaddr_in sockAddr;
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_port = htons(port);
+    sockAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+    if (bind(serverSocket, (sockaddr *)&sockAddr, sizeof(sockAddr)) == SOCKET_ERROR) {
+        printf("bind server error!\n");
+        return false;
+    }
+    if (listen(serverSocket, 10) == SOCKET_ERROR) {
+        printf("create server listenner error!\n");
+        return false;
+    }
+    return true;
 }
 
-bool SocketServerTCP::connect()
-{
-	sockaddr_in client_sin;
-	int len = sizeof(client_sin);
-	connectionSocket = accept(serverSocket, (sockaddr*)&client_sin, &len);
-	if (connectionSocket == INVALID_SOCKET) {
-		printf("create connection error!\n");
-		return false;
-	}
-	return true;
+bool SocketServerTCP::connect() {
+    sockaddr_in client_sin;
+    int len = sizeof(client_sin);
+    connectionSocket = accept(serverSocket, (sockaddr *)&client_sin, &len);
+    if (connectionSocket == INVALID_SOCKET) {
+        printf("create connection error!\n");
+        return false;
+    }
+    return true;
 }
 
-void SocketServerTCP::disconnect()
-{
-	closesocket(connectionSocket);
+void SocketServerTCP::disconnect() {
+    closesocket(connectionSocket);
 }
 
-
-
-bool SocketServerTCP::receive(char buffer[], int buffer_size, SOCKETDATATYPE &type, int &size)
-{
-	size = recv(connectionSocket, buffer, buffer_size, 0);
-	if (size < 0) {
-		printf("server receive data failed!\n");
-		return false;
-	}
-	if (buffer[0] == '#') {	//Ω” ’µΩÕº∆¨
-		type = IMAGE_DATA;
-		size--;
-		for (int i = 0; i < size; i++) {
-			buffer[i] = buffer[i + 1];
-		}
-	}
-	else if (buffer[0] == '*') {	//Ω” ’µΩøÿ÷∆–≈∫≈
-		type = CONTROL_DATA;
-		size--;
-		for (int i = 0; i < size; i++) {
-			buffer[i] = buffer[i + 1];
-		}
-	}
-	else {
-		printf("unknown SOCKETDATATYPE!\n");
-		return false;
-	}
-	return true;
+bool SocketServerTCP::receive(char *buffer, int buffer_size, SOCKETDATATYPE &type, int &size) {
+    size = recv(connectionSocket, buffer, buffer_size, 0);
+    if (size < 0) {
+        printf("server receive data failed!\n");
+        return false;
+    }
+    if (buffer[0] == '#') {
+        type = IMAGE_DATA;
+        size--;
+        for (int i = 0; i < size; i++) {
+            buffer[i] = buffer[i + 1];
+        }
+    } else if (buffer[0] == '*') {
+        type = CONTROL_DATA;
+        size--;
+        for (int i = 0; i < size; i++) {
+            buffer[i] = buffer[i + 1];
+        }
+    } else {
+        printf("unknown SOCKETDATATYPE!\n");
+        return false;
+    }
+    return true;
 }
 
-void SocketServerTCP::cleanup()
-{
-	closesocket(serverSocket);
-	WSACleanup();
+void SocketServerTCP::cleanup() {
+    closesocket(serverSocket);
+    WSACleanup();
 }
 
-int main()
-{
-	SocketServerTCP ss;
-	ss.init(8086);
-	Mat image;
-	while (true) {
-		ss.connect();
-		char buffer[100000];
-		SOCKETDATATYPE type;
-		int size;
-		ss.receive(buffer, sizeof(buffer), type, size);
-		if (type == IMAGE_DATA) {
-			std::vector<uchar> image_decode;
-			int idx = 0;
-			while (idx < size) {
-				image_decode.push_back(uchar(buffer[idx++]));
+int main() {
+    SocketServerTCP ss;
+    ss.init(8086);
+    Mat image;
+    while (true) {
+        ss.connect();
+		// Êï∞ÊçÆÊé•Êî∂ÁºìÂÜ≤Âå∫
+        char buffer[100000];
+        SOCKETDATATYPE type;
+        int size;
+        ss.receive(buffer, sizeof(buffer), type, size);
+        if (type == IMAGE_DATA) {
+            std::vector<uchar> image_decode;
+            int idx = 0;
+            while (idx < size) {
+                image_decode.push_back(uchar(buffer[idx++]));
+            }
+            Mat image = imdecode(image_decode, CV_LOAD_IMAGE_COLOR);
+            imshow("image", image);
+            waitKey(30);
+        } else if(type == CONTROL_DATA){
+			for(int i=0; i< size; i++){
+				printf("%s", buffer[i]);
 			}
-			Mat image = imdecode(image_decode, CV_LOAD_IMAGE_COLOR);
-			imshow("image2", image);
-			waitKey(30);
-		}
-		else {
-			printf("unknown SOCKETDATATYPE!");
-		}
-		ss.disconnect();
-	}
-	ss.cleanup();
-	return 0;
+		} else {
+            printf("unknown SOCKETDATATYPE!");
+        }
+        ss.disconnect();
+    }
+    ss.cleanup();
+    return 0;
 }
