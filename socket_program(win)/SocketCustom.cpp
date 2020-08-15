5124086d87af6b151fbc88a1e3606e97da3753da
kyn0v <2275169040@qq.com>
2020-08-15 21:59:50 +0800

    ADD:添加UDP功能

--- socket_program(win)/SocketCustom.cpp
+++ socket_program(win)/SocketCustom.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 using namespace cv;
 
-//--------------------Client请求---------------------
+//--------------------Client请求(TCP)---------------------
 bool SocketClientTCP::init(string ip, int port)
 {
 	WORD sockVersion = MAKEWORD(2, 2);
@@ -13,10 +13,9 @@ bool SocketClientTCP::init(string ip, int port)
 		printf("WSAStartup error!\n");
 		return false;
 	}
-
-	sock_in.sin_family = AF_INET;
-	sock_in.sin_port = htons(port);
-	sock_in.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+	clientAddr.sin_family = AF_INET;
+	clientAddr.sin_port = htons(port);
+	clientAddr.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
 	return true;
 }
 
@@ -27,7 +26,7 @@ bool SocketClientTCP::connnect()
 		printf("create client error!\n");
 		return false;
 	}
-	if (connect(clientSocket, (sockaddr*)&sock_in, sizeof(sock_in)) == SOCKET_ERROR) {
+	if (connect(clientSocket, (sockaddr*)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
 		printf("client connect error!\n");
 		return false;
 	}
@@ -43,7 +42,7 @@ bool SocketClientTCP::transmit(char * data, int size, SOCKETDATATYPE type)
 {
 	if (type == IMAGE_DATA) {	// 发送图像数据
 		char image_buffer[100000];
-		char flag[10];	// 标志位：# + 数据长度
+		char flag[10];	// 标志位'#'1位 + 数据长度9位
 		sprintf(flag, "#%09d", size);
 		for (int i = 0; i < 10; ++i) {
 			image_buffer[i] = flag[i];
@@ -57,10 +56,26 @@ bool SocketClientTCP::transmit(char * data, int size, SOCKETDATATYPE type)
 			return false;
 		}
 	}
-	else if (type == PTZINFO_DATA) {	// 发送控制信号数据
-		char control_buffer[1000];
-		char flag[10];	// 标志位：* + 数据长度
-		sprintf(flag, "*%09d", size);
+	else if (type == PTZINFO_DATA) {	// 发送云台信息
+		char control_buffer[50];
+		char flag[10];
+		sprintf(flag, "*%09d", size); 	// 标志位'*'1位 + 数据长度9位 
+		for (int i = 0; i < 10; ++i) {
+			control_buffer[i] = flag[i];
+		}
+		for (int i = 0; i < size; i++)
+		{
+			control_buffer[i + 10] = data[i];
+		}
+		if (send(clientSocket, control_buffer, size + 10, 0) < 0) {
+			printf("send control data error!\n");
+			return false;
+		}
+	}
+	else if (type == CTRL_DATA) {	// 发送控制信号
+		char control_buffer[50];
+		char flag[10];
+		sprintf(flag, "@%09d", size);	// 标志位'@'1位 + 数据长度9位
 		for (int i = 0; i < 10; ++i) {
 			control_buffer[i] = flag[i];
 		}
@@ -82,13 +97,11 @@ bool SocketClientTCP::transmit(char * data, int size, SOCKETDATATYPE type)
 
 bool SocketClientTCP::receive(char *buffer, int buffer_size)
 {
-	//int ret = recv(clientSocket, buffer, buffer_size, 0);
-	//if (ret>0) {
-	//	buffer[ret] = 0x00;
-	//	printf(buffer);
-	//	return true;
-	//}
-	//return false;
+	int ret = recv(clientSocket, buffer, buffer_size, 0);
+	if (ret > 0) {
+		// You can test the received data here.
+		return true;
+	}
 	return false;
 }
 
@@ -97,7 +110,7 @@ void SocketClientTCP::cleanup()
 	WSACleanup();
 }
 
-//--------------------Server监听---------------------
+//--------------------Server监听(TCP)---------------------
 bool SocketServerTCP::init(int port)
 {
 	WORD sockVersion = MAKEWORD(2, 2);
@@ -130,9 +143,8 @@ bool SocketServerTCP::init(int port)
 
 bool SocketServerTCP::connect()
 {
-	sockaddr_in client_sin;
-	int len = sizeof(client_sin);
-	connectionSocket = accept(serverSocket, (sockaddr*)&client_sin, &len);
+	int len = sizeof(clientAddr);
+	connectionSocket = accept(serverSocket, (sockaddr*)&clientAddr, &len);
 	if (connectionSocket == INVALID_SOCKET) {
 		printf("create connection error!\n");
 		return false;
@@ -147,14 +159,15 @@ void SocketServerTCP::disconnect()
 
 bool SocketServerTCP::transmit(char * data, int size)
 {
-	if (send(connectionSocket, data, size, 0) < 0) {
+	int ret = send(connectionSocket, data, size, 0);
+	if (ret < 0) {
 		printf("send control data error!\n");
 		return false;
 	}
 	return true;
 }
 
-bool SocketServerTCP::receive(char buffer[], int buffer_size, SOCKETDATATYPE &type, int &size)
+bool SocketServerTCP::receive(char *buffer, int buffer_size, SOCKETDATATYPE &type, int &size)
 {
 	int ret = recv(connectionSocket, buffer, buffer_size, 0);
 	char size_str[9];
@@ -181,6 +194,12 @@ bool SocketServerTCP::receive(char buffer[], int buffer_size, SOCKETDATATYPE &ty
 			buffer[i] = buffer[i + 10];
 		}
 	}
+	else if (buffer[0] == '@') {	//接收到控制信号
+		type = CTRL_DATA;
+		for (int i = 0; i < size; i++) {
+			buffer[i] = buffer[i + 10];
+		}
+	}
 	else {
 		printf("unknown SOCKETDATATYPE!\n");
 		return false;
@@ -189,6 +208,108 @@ bool SocketServerTCP::receive(char buffer[], int buffer_size, SOCKETDATATYPE &ty
 }
 
 void SocketServerTCP::cleanup()
+{
+	closesocket(serverSocket);
+	WSACleanup();
+}
+
+//--------------------Client请求(UDP)---------------------
+bool SocketClientUDP::init(string ip, int port)
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA data;
+	if (WSAStartup(sockVersion, &data) != 0)
+	{
+		printf("WSAStartup error!\n");
+		return false;
+	}
+	clientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (clientSocket == INVALID_SOCKET) {
+		printf("create client error!\n");
+		return false;
+	}
+	clientAddr.sin_family = AF_INET;
+	clientAddr.sin_port = htons(port);
+	clientAddr.sin_addr.S_un.S_addr = inet_addr(ip.c_str());
+	return true;
+}
+
+bool SocketClientUDP::transmit(char * data, int size)
+{
+	int ret = sendto(clientSocket, data, size, 0, (sockaddr *)&clientAddr, sizeof(clientAddr));
+	if (ret < 0) {
+		printf("send data error!\n");
+		return false;
+	}
+	return true;
+}
+
+bool SocketClientUDP::receive(char *buffer, int buffer_size, int &recv_size)
+{
+	int addrLen = sizeof(clientAddr);
+	recv_size = recvfrom(clientSocket, buffer, 255, 0, (sockaddr *)&clientAddr, &addrLen);
+	if (recv_size<0) {
+		return false;
+	}
+	return true;
+}
+
+void SocketClientUDP::cleanup()
+{
+	closesocket(clientSocket);
+	WSACleanup();
+}
+
+//--------------------Server监听(UDP)---------------------
+bool SocketServerUDP::init(int port)
+{
+	WORD sockVersion = MAKEWORD(2, 2);
+	WSADATA wsdata;
+	if (WSAStartup(sockVersion, &wsdata) != 0)
+	{
+		printf("WSAStartup error!\n");
+		return false;
+	}
+	serverSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (serverSocket == INVALID_SOCKET)
+	{
+		printf("create server error!\n");
+		return false;
+	}
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+		printf("bind server error!\n");
+		closesocket(serverSocket);
+		return false;
+	}
+	return true;
+}
+
+bool SocketServerUDP::transmit(char *data, int size)
+{
+	int addrLen = sizeof(clientAddr);
+	int ret = sendto(serverSocket, data, size, 0, (sockaddr *)&clientAddr, sizeof(clientAddr));
+	if (ret < 0) {
+		printf("send data error!\n");
+		return false;
+	}
+	return true;
+}
+
+bool SocketServerUDP::receive(char *buffer, int buffer_size, int &recv_size)
+{
+	int addrLen = sizeof(clientAddr);
+	recv_size = recvfrom(serverSocket, buffer, buffer_size, 0, (sockaddr *)&clientAddr, &addrLen);
+	if (recv_size > 0) {
+		// You can test the received data here.
+		return true;
+	}
+	return false;
+}
+
+void SocketServerUDP::cleanup()
 {
 	closesocket(serverSocket);
 	WSACleanup();
--- socket_program(win)/SocketCustom.h
+++ socket_program(win)/SocketCustom.h
@@ -1,6 +1,14 @@
+/*******************************************************
+Socket封装类
+
+Created by kyn0v.
+2020.08.15
+********************************************************/
+
 #pragma once
 #define  _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>  
@@ -12,7 +20,8 @@ using namespace std;
 enum SOCKETDATATYPE
 {
 	IMAGE_DATA = 1,	// 图像帧标志位#
-	PTZINFO_DATA = 2 // 云台信息标志位*
+	PTZINFO_DATA = 2, // 云台信息标志位*
+	CTRL_DATA = 3 // 云台控制信号标志位%
 };
 
 typedef struct {
@@ -21,6 +30,12 @@ typedef struct {
 	int zoom = 3;
 	int focus = 4;
 } PTZInfo;
+
+typedef struct {
+	int cmd;
+	int stop;
+	int speed;
+} PTZCtrl;
 
 class SocketClientTCP
 {
@@ -33,7 +48,7 @@ public:
 	void cleanup();
 private:
 	SOCKET clientSocket;
-	sockaddr_in sock_in;
+	sockaddr_in clientAddr;
 };
 
 class SocketServerTCP
@@ -43,10 +58,37 @@ public:
 	bool connect();
 	void disconnect();
 	bool transmit(char *data, int size);
-	bool receive(char buffer[], int buffer_size, SOCKETDATATYPE &type, int &size);
+	bool receive(char *buffer, int buffer_size, SOCKETDATATYPE &type, int &size);
 	void cleanup();
 private:
 	SOCKET serverSocket;
 	SOCKET connectionSocket;
+	sockaddr_in clientAddr;
+};
+
+//------------------------- 注：TCP面向连接，而UDP是无连接的-------------------
+class SocketClientUDP
+{
+public:
+	bool init(string ip, int port);
+	bool transmit(char *data, int size);
+	bool receive(char *buffer, int buffer_size, int &recv_size);
+	void cleanup();
+private:
+	SOCKET clientSocket;
+	sockaddr_in clientAddr;
+};
+
+class SocketServerUDP
+{
+public:
+	bool init(int port);
+	bool transmit(char *data, int size);
+	bool receive(char *buffer, int buffer_size, int &ret_size);
+	void cleanup();
+private:
+	SOCKET serverSocket;
+	sockaddr_in serverAddr;
+	sockaddr_in clientAddr;
 };
 
