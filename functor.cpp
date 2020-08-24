92a83b1882cd4237f6ea540b710c5adc4d86c593
kyn0v <2275169040@qq.com>
2020-08-24 16:10:34 +0800

    add: functor(仿函数) example

---
+++ functor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+class greaterCustom {
+   public:
+    greaterCustom(T x) { threshold = x; }
+    bool operator()(T x) const { return x > threshold; }
+
+   private:
+    T threshold;
+};
+
+int main() {
+    int n, threshold, cnt;
+    cin>>n>>threshold;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    greaterCustom<int> functor(threshold);
+    cnt = count_if(nums.begin(), nums.end(), functor);
+    cout << cnt << endl;
+    return 0;
+}
